bfc3358ba43cc2779a6bd1549aeb277dd52ae764
your_name <2322754493@qq.com>
2020-02-03 22:20:35 +0800

    import { fromJS } from immutable;

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -63,7 +63,7 @@ const Header = (props) => {
  */
 const mapStateToProps = (state) => {
     return {
-        focused: state.header.focused
+        focused: state.header.get('focused')
     }
 }
 /*
--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,19 +1,16 @@
 import * as constants from './constants';
+import { fromJS } from 'immutable'; 
 
-const defaultStore = {
+const defaultStore = fromJS({
     focused: false
-}
+});
 
 export default (state = defaultStore, action) => {
     if(action.type === constants.SEARCH_FOCUS) {
-        return {
-            focused: true
-        }
+        return state.set('focused', true);
     }
     if(action.type === constants.SEARCH_BLUR) {
-        return {
-            focused: false
-        }
+        return state.set('focused', false);
     }
     return state;
 }
