94dbaad32320cd02ffc4b66f081a872e62db5996
your_name <2322754493@qq.com>
2020-02-03 21:08:47 +0800

    actionTypes.js

---
+++ src/common/header/store/index.js
@@ -0,0 +1,5 @@
+import headerReducer from './reducer';
+import * as constants from './constants';
+import * as actionCreators from './actionCreators';
+
+export { headerReducer, constants, actionCreators }
--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,14 +1,16 @@
+import * as constants from './constants';
+
 const defaultStore = {
     focused: false
 }
 
 export default (state = defaultStore, action) => {
-    if(action.type === 'search_focus') {
+    if(action.type === constants.SEARCH_FOCUS) {
         return {
             focused: true
         }
     }
-    if(action.type === 'search_blur') {
+    if(action.type === constants.SEARCH_BLUR) {
         return {
             focused: false
         }
