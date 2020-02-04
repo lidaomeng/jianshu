08b2292e0d186e2aab02ea1a97bfd127a201f554
your_name <2322754493@qq.com>
2020-02-04 19:17:02 +0800

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {GlobalStyled} from './style';
 import {GlobalStyled2} from './statics/iconfont/iconfont';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 import store from './store';
 
 import Header from './common/header'
@@ -10,9 +11,18 @@ class App extends Component{
   render() {
     return (
         <Provider store={store}>
-            <GlobalStyled />
-            <GlobalStyled2 />
-            <Header />
+            <div>
+              <GlobalStyled />
+              <GlobalStyled2 />
+              <Header />
+
+              <BrowserRouter>
+                <div>
+                  <Route path='/' exact render={() => <div>home</div>}></Route>
+                  <Route path='/detail' exact render={() => <div>detail</div>}></Route>
+                </div>
+              </BrowserRouter>
+            </div>
         </Provider>
     )
   }
